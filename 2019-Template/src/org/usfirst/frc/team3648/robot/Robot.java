6050e6f1132810de8e58f01dd9eac6027c616756
685228 <685228@457-033-L19.schools.dpsk12.org>
2018-09-06 13:50:28 -0600

    Add base code for sparks, buttons, and such.

---
+++ 2019-Template/src/org/usfirst/frc/team3648/robot/Robot.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team3648.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Robot extends IterativeRobot {
+	private Joystick driveController = new Joystick(0);
+	
+	private Spark frontLeft = new Spark(2);
+	private Spark backLeft = new Spark(0);
+	private Spark frontRight = new Spark(3);
+	private Spark backRight = new Spark(1);
+	SpeedControllerGroup leftSide = new SpeedControllerGroup(frontLeft, backLeft);
+	SpeedControllerGroup rightSide = new SpeedControllerGroup(frontRight, backRight);
+	
+	private DifferentialDrive drive = new DifferentialDrive(leftSide, rightSide);
+	
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		
+	}
+
+	@Override
+	public void autonomousInit() {
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+	}
+
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+}
